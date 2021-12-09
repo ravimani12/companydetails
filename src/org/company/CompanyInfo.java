3aa8d90a675d514609e5f509e93bde0bb47be7ae
ravimani12 <manikandan30796@gmail.com>
2021-12-09 20:09:13 +0530

    done my task

---
+++ src/org/company/CompanyInfo.java
@@ -0,0 +1,26 @@
+package org.company;
+
+public class CompanyInfo {
+
+	private void companyName() {
+		System.out.println("DineshIndustry");
+	}
+	private void companyId() {
+		System.out.println("dinu12");
+	}
+	private void companyAddress() {
+		System.out.println("first st,kalavasal,periyar,madurai");
+
+}
+	public static void main(String[] args) {
+		CompanyInfo c = new CompanyInfo();
+		c.companyName();
+		c.companyId();
+		c.companyAddress();
+	}
+	
+	
+}
+
+
+
---
+++ src/org/company/LanguageInfo.java
@@ -0,0 +1,10 @@
+package org.company;
+
+public class LanguageInfo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
